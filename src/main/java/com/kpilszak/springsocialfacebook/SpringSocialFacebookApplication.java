bf90f27284bcb8fd806827583d33309ffd9d619d
kpilszak <klaudia.pilszak@gmail.com>
2020-11-03 19:48:10 +0100

    Create repo

---
+++ src/main/java/com/kpilszak/springsocialfacebook/SpringSocialFacebookApplication.java
@@ -0,0 +1,13 @@
+package com.kpilszak.springsocialfacebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialFacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialFacebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpilszak/springsocialfacebook/SpringSocialFacebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpilszak.springsocialfacebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocialFacebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
