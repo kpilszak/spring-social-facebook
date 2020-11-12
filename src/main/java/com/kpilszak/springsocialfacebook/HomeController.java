2710b3fb3851eddc51bdef8e290427407a5e0f2e
kpilszak <klaudia.pilszak@gmail.com>
2020-11-12 07:55:46 +0100

    feat: Add dependencies to pom.xml

--- src/main/java/com/kpilszak/springsocialfacebook/HomeController.java
+++ src/main/java/com/kpilszak/springsocialfacebook/HomeController.java
@@ -1,9 +1,17 @@
 package com.kpilszak.springsocialfacebook;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final static Logger LOG = LoggerFactory.getLogger(HomeController.class);
+
+    private final Facebook facebook;
+
+
 }
