7e0d3018fda38ef7e22d86955d2098ee81f05a9d
kpilszak <klaudia.pilszak@gmail.com>
2020-11-16 19:41:54 +0100

    feat: Set SocialConfig to @EnableSocial to have the Spring MVC configuration defined

--- src/main/java/com/kpilszak/springsocialfacebook/SocialConfig.java
+++ src/main/java/com/kpilszak/springsocialfacebook/SocialConfig.java
@@ -1,4 +1,10 @@
 package com.kpilszak.springsocialfacebook;
 
-public class SocialConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.social.config.annotation.EnableSocial;
+import org.springframework.social.config.annotation.SocialConfigurerAdapter;
+
+@Configuration
+@EnableSocial
+public class SocialConfig extends SocialConfigurerAdapter {
 }
