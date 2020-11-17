d3bba253d7eca4d5697d617105f4faec4b992f45
kpilszak <klaudia.pilszak@gmail.com>
2020-11-17 19:05:45 +0100

    feat: Create addConnectionFactories

--- src/main/java/com/kpilszak/springsocialfacebook/SocialConfig.java
+++ src/main/java/com/kpilszak/springsocialfacebook/SocialConfig.java
@@ -1,10 +1,26 @@
 package com.kpilszak.springsocialfacebook;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 
 @Configuration
 @EnableSocial
 public class SocialConfig extends SocialConfigurerAdapter {
+
+    @Override
+    public void addConnectionFactories(
+            ConnectionFactoryConfigurer config,
+            Environment env
+    ) {
+        config.addConnectionFactory(
+                new FacebookConnectionFactory(
+                        env.getProperty("facebook.app.id"),
+                        env.getProperty("facebook.app.secret")
+                )
+        );
+    }
 }
