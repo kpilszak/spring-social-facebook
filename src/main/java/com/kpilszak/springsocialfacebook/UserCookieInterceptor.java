1c1cd8198c0412e0ece4d73a3f4a48199c65d8ec
kpilszak <klaudia.pilszak@gmail.com>
2020-11-28 21:41:33 +0100

    feat: Add logger to UserCookieInterceptor

--- src/main/java/com/kpilszak/springsocialfacebook/UserCookieInterceptor.java
+++ src/main/java/com/kpilszak/springsocialfacebook/UserCookieInterceptor.java
@@ -1,6 +1,11 @@
 package com.kpilszak.springsocialfacebook;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class UserCookieInterceptor extends HandlerInterceptorAdapter {
+
+    private final static Logger LOG =
+            LoggerFactory.getLogger(UserCookieInterceptor.class);
 }
