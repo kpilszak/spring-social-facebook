7ca5a78337c87393a605c0adb7850e083242d41a
kpilszak <klaudia.pilszak@gmail.com>
2020-11-29 07:13:07 +0100

    feat: Add UsersConnectionRepository to UserCookieInterceptor

--- src/main/java/com/kpilszak/springsocialfacebook/UserCookieInterceptor.java
+++ src/main/java/com/kpilszak/springsocialfacebook/UserCookieInterceptor.java
@@ -2,10 +2,13 @@ package com.kpilszak.springsocialfacebook;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class UserCookieInterceptor extends HandlerInterceptorAdapter {
 
     private final static Logger LOG =
             LoggerFactory.getLogger(UserCookieInterceptor.class);
+    
+    private final UsersConnectionRepository repository;
 }
